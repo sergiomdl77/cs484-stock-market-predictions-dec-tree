902987f3c3d60ccbce51be7239efbd3707228d82
saad <saadarshad2012@hotmail.com>
2020-04-28 15:36:36 -0400

    Pre-Processed Data

--- finalProject.py
+++ finalProject.py
@@ -34,39 +34,61 @@ from sklearn.neural_network import MLPClassifier
 def main():
 
     print('Welcome to Stock Portfolio Builder\n')
-    userName = input('What Username would you like you use\n')
-    log = open('userName', 'w')
+    # userName = input('What Username would you like you use for you Portfolio\n')
+    # log = open('userName', 'w')
 
-    trainHolder = []
-    trainTitle = []
+    trainHolder = []    #Holds all of the stocks as vectors
+    trainTitle = []     #Holds the first row which is the attributes
+    trainNames = []     #Holds the names of the stocks, first colum
+    trainLabel = []     #Holds the labels of each stock
+
     testHolder = []
     testTitle = []
+    testNames = []
+    testLabel = []
 
     counter = 0
     # 2017 Will be used as our TEST file
     for line in open('2017_Financial_Data.dat', 'r'):
         if counter == 0:
             line = line.split(',')
+            line = line[: len(line) - 5]
             trainTitle = line
             counter += 1
         else:
             line = line.split(',')
+            line = line[: len(line) - 4]
+            trainNames.append(line[0])
+            del line[0]
+            for s in range(len(line)):
+                if line[s] == '':
+                    line[s] = 0
+                line[s] = float(line[s])
+            trainLabel.append(line[1])
+            del line[1]
             trainHolder.append(line)
 
-    print(len(trainTitle))
-    print(trainTitle)
-    print(len(trainHolder[0]))
-    print(trainHolder[0])
+    print(trainLabel)
 
     tcounter = 0
     # 2016 Will be used as our TRAIN file
     for line in open('2016_Financial_Data.dat', 'r'):
         if tcounter == 0:
             line = line.split(',')
-            testTitle.append(line)
+            line = line[: len(line) - 5]
+            testTitle = line
             tcounter += 1
         else:
             line = line.split(',')
+            line = line[: len(line) - 4]
+            testNames.append(line[0])
+            del line[0]
+            for s in range(len(line)):
+                if line[s] == '':
+                    line[s] = 0
+                line[s] = float(line[s])
+            testLabel.append(line[1])
+            del line[1]
             testHolder.append(line)
 
 
